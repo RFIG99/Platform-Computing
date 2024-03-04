9562f82742b76c2a28b9c12778b6816df41141fc
RFIG99 <126642130+RFIG99@users.noreply.github.com>
2024-03-04 09:56:37 -0800

    lab 2

--- Lab 2/my-app/src/App.js
+++ Lab 2/my-app/src/App.js
@@ -4,21 +4,52 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          420yoloswag for jesus
-        </a>
+      <header className="About me">
+
+      <body>
+<div class = "mainHDR">
+<h1>About Me</h1>
+</div>
+
+
+<div class = "paragraph">
+
+<div class = "paraHDR">
+<h1>Life</h1>
+</div>
+
+<p>I have lived in San Bernardino my whole life. I attended Cajon High School and San Bernardino Valley College Before I came to CSUSB. In high school, I played water polo and was on the swim team. </p>
+
+
+<div class = "paraHDR">
+<h1>Food</h1>
+</div>
+
+<p> My favorite food is pizza. I also like spaghetti and taquitos. I also like blue cheese burgers from Red Robbin.</p>
+
+<div class = "paraHDR">
+<h1>Games</h1>
+</div>
+
+<p>I like to play Arkham Batman series of games. I also like to play Metal Gear Solid 5 and games from the Legend of Zelda series. I also play Halo Master Chief Collection and Mortal Kombat 11.</p>
+</div>
+
+<div class = "image">
+
+<img src="funny_meme.jpg" alt="Funny meme go here" ></img>
+
+</div>
+
+<div class = "git">
+
+<a href="https://github.com/RFIG99/Platform-Computing" target="_blank"> Welcome to my Platform Computing Github</a>
+
+</div>
+
+</body>
       </header>
     </div>
+    
   );
 }
 
